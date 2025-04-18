72941715f696ac6641df526a08800be5f4c7ce2c
子良 耿 <Gengziliang25@outlook.com>
2025-04-18 20:18:01 +0800

    添加三维实景

---
+++ Dyh/scripts/live3D.js
@@ -0,0 +1,26 @@
+function addReality3DData(){
+    const tileset = viewer.scene.primitives.add(
+        new Cesium.Cesium3DTileset({
+            url: "./3dTiles/3dTiles1.0/tileset.json",
+            maximumScreenSpaceError: 32,
+            //- 直接跳转到所需的详细层级，避免加载低分辨率过渡瓦片，适用于提升性能和用户体验的场景
+            skipLevelOfDetail: true,
+            immediatelyLoadDesiredLevelOfDetail: true,
+        })
+    );
+    return tileset;
+}
+
+let tileset = null;
+$('#add3DData').on('change', function () {
+    if ($(this).is(':checked')) {
+        //被勾选了，执行加载实景三维逻辑
+        console.log('加载实景三维数据');
+        tileset = addReality3DData();
+        viewer.zoomTo(tileset);
+    } else {
+        //取消勾选，执行移除实景三维逻辑
+        console.log('移除实景三维数据');
+        viewer.scene.primitives.remove(tileset);
+    }
+});
--- Dyh/scripts/main.js
+++ Dyh/scripts/main.js
@@ -26,3 +26,4 @@ viewer.camera.setView({
 
 
 
+
